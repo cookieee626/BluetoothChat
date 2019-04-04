76e5b985598404fced062735580fae631bdd32fd
cookieee626 <cookieee.626+tech@gmail.com>
2019-04-05 07:00:26 +0900

    add comment

--- Application/src/main/java/com/example/android/bluetoothchat/MainActivity.java
+++ Application/src/main/java/com/example/android/bluetoothchat/MainActivity.java
@@ -36,6 +36,7 @@ import com.example.android.common.logger.MessageOnlyLogFilter;
  * For devices with displays with a width of 720dp or greater, the sample log is always visible,
  * on other devices it's visibility is controlled by an item on the Action Bar.
  */
+// ここに紐付ける
 public class MainActivity extends SampleActivityBase {
 
     public static final String TAG = "MainActivity";
